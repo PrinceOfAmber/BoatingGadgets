fa6c203307209d7357ee8a3e1e5235054dfd4c4f
Sam <lothrazar@hotmail.com>
2018-10-16 20:45:47 -0700

    initial forge 1.12.2 setup

---
+++ src/main/java/com/lothrazar/boatgadgets/ModBoating.java
@@ -0,0 +1,29 @@
+package com.lothrazar.boatgadgets;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ModBoating.MODID )
+public class ModBoating
+{
+    public static final String MODID = "boatgadgets"; 
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
